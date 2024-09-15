71c296dc3e1bd74174b717e042aa6d793d1516a7
Tanmoy <majitanmoy14@gmail.com>
2024-09-16 00:06:45 +0530

    sep 16

--- basic.cpp
+++ basic.cpp
@@ -1,17 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[] = {3,2,1,3,5,6,8};
-    int n = sizeof(arr)/sizeof(int);
-    int i=0;
-    int j = 0;;
-    int pre[n];
-    while(i<n){
-        j+=arr[i];
-        pre[i++]=j;
+    vector<int> nums{-1,-8,0,5,-9};
+    int n = nums.size();
+    sort(nums.begin(),nums.end());
+    //sufix sum
+    for(int i=n-2;i>=0;i--){
+        nums[i]=nums[i+1]+nums[i];
     }
+
+    int idx = -1;
     for(int i=0;i<n;i++){
-        cout<<pre[i]<<" ";
+        if(nums[i]>=0){
+            idx= i;
+            break;
+        }
     }
+  int p = 1;
+  int ans = 0;
+  for(int i=idx;i<n;i++){
+     ans+=nums[i]*p;
+     p++;
+  }
+  cout<<"Final ans is : "<<ans<<endl;
     return 0;
 }
